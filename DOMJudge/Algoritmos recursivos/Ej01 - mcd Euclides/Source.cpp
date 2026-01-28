1e91a32a4a0481e60555c5ed4ab99dcb7c83d82c
brunoloz <brunoloz@ucm.es>
2026-01-28 11:47:49 +0100

    ejercicio 1 resuelto

---
+++ DOMJudge/Algoritmos recursivos/Ej01 - mcd Euclides/Source.cpp
@@ -0,0 +1,57 @@
+﻿// Nombre del alumno ..... Bruno Lozano Clemente
+// Usuario del Juez ...... FAL-J19
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+
+// función que resuelve el problema
+int mcd(int a, int b) {
+
+    if (b == 0) return a;
+
+    return mcd(b, a % b);
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int a, b;
+    std::cin >> a >> b;
+    if (a == 0 && b == 0)
+        return false;
+
+    int sol = mcd(a,b);
+
+    // escribir sol
+    std::cout << sol << '\n';
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
